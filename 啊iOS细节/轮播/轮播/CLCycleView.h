00a2eced3cc2954c2faf4ea8708a3e7934fa81bf
mac <1263038178@qq.com>
2019-04-30 17:59:55 +0800

    commit

---
+++ 啊iOS细节/轮播/轮播/CLCycleView.h
@@ -0,0 +1,17 @@
+//
+//  CLCycleView.h
+//  轮播
+//
+//  Created by chenxl on 2019/4/30.
+//  Copyright © 2019 chenxl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CLCycleView : UICollectionView
+@property(nonatomic,strong) NSArray *imageArray;
+@end
+
+NS_ASSUME_NONNULL_END
--- 啊iOS细节/轮播/轮播/ViewController.m
+++ 啊iOS细节/轮播/轮播/ViewController.m
@@ -7,9 +7,8 @@
 //
 
 #import "ViewController.h"
-
+#define SCREEN_WIDTH [UIScreen mainScreen].bounds.size.width
 @interface ViewController ()
-
 @end
 
 @implementation ViewController
@@ -17,7 +16,49 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+ 
 }
 
+
+
+
+
+
+//-(UIScrollView *)scrollView{
+//    if (_scrollView == nil) {
+//        _scrollView = [[UIScrollView alloc]initWithFrame:CGRectMake(0, 100, SCREEN_WIDTH, 300)];
+//        _scrollView.contentInset = UIEdgeInsetsMake(0, 0, 0, 0);
+//        _scrollView.delegate = self;
+//        _scrollView.pagingEnabled = YES;
+//        _scrollView.backgroundColor = [UIColor greenColor];
+//        _scrollView.showsHorizontalScrollIndicator = NO;
+//        _scrollView.decelerationRate = 1.0;
+//        _scrollView.contentOffset = CGPointMake(0, 0);
+//        _scrollView.contentSize = CGSizeMake(SCREEN_WIDTH*3,0);
+//    }
+//    return _scrollView;
+//}
+//- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+//
+//}
+//
+//- (void)scrollViewDidEndScrollingAnimation:(UIScrollView *)scrollView {
+//
+//
+//}
+//#pragma mark -- 拖拽停止NSTimer
+//- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
+//
+//
+//
+//}
+//
+//- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
+//{
+//
+//
+//}
 
 @end
